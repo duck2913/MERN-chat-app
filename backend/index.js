febcf896d0420235a36796f28b348f4ad15c9de8
duck2913 <minhducdl1010@gmail.com>
2022-06-05 17:44:38 +0700

    init

---
+++ backend/index.js
@@ -0,0 +1,24 @@
+const app = require("express")();
+const server = require("http").createServer(app);
+const cors = require("cors");
+
+const io = require("socket.io")(server, {
+	cors: {
+		origin: "*",
+		methods: ["GET", "POST"],
+	},
+});
+
+app.use(cors());
+
+const PORT = process.env.PORT || 5000;
+
+app.get("/", (req, res) => {
+	res.send("Server is running");
+});
+
+io.on("connection", (socket) => {});
+
+server.listen(PORT, () => {
+	console.log(`server is listening on port ${PORT}`);
+});
